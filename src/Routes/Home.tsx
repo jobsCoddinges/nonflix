3680e419214e10d5b8b878ad0a262bc24832d9fd
gyuchul <tlsrbcjf7579@naver.com>
2022-06-15 16:22:15 +0900

    nonflix

---
+++ src/Routes/Home.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div style={{ backgroundColor: "white", height: "200vh" }}>Home</div>;
+};
+
+export default Home;
---
+++ src/Routes/Tv.tsx
@@ -0,0 +1,5 @@
+const Tv = () => {
+  return <h1>tv</h1>;
+};
+
+export default Tv;
